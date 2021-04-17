6e291c37d16b6015736c8ef90c76d0ce3f493a9d
Luiz <lzullinojuni2020@fau.edu>
2021-04-16 22:30:39 -0400

    updated project

--- Overtake/Assets/Scripts/PlayerMovement.cs
+++ Overtake/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    //public BoxCollider2D boxCollider;
     public CircleCollider2D circleCollider;
     public Animator animator;
     [SerializeField] private LayerMask layerMask;
@@ -17,13 +17,23 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
     bool isDead = false;
     bool isFinished = false;
+    
+    Vector3 checkpoint;
+    Transform playerTransform;
+
+    public GameObject timerObj;
+    public GameObject scoreObj;
+    float timer;
+
+    int scoreMultiplier = 10;
+    int maxScore = 1000;
 
     public SceneLoader sceneLoader;
     public float deathBoundary;
     public UnityEvent OnDeath;
     public UnityEvent OnFinish;
 
-    private void Awake()
+    void Awake()
     {
         if (OnDeath == null)
             OnDeath = new UnityEvent();
@@ -31,13 +41,21 @@ public class PlayerMovement : MonoBehaviour
         if (OnFinish == null)
             OnFinish = new UnityEvent();
     }
+    void Start()
+    {
+        //Get Trasnform and updated checkpoint position
+        playerTransform = this.GetComponent<Transform>();
+        checkpoint = playerTransform.position;
+        timer = 0;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         //Check collision with Obstacles
         if (collision.gameObject.name == "Obstacles")
         {
             isDead = true;
-            OnDeath.Invoke();
+            //OnDeath.Invoke();
+            Respawn();
         }
 
         //Check collision with Coin for winning/finish line
@@ -48,37 +66,54 @@ public class PlayerMovement : MonoBehaviour
 
             collision.gameObject.SetActive(false);
             isFinished = true;
+            scoreObj.GetComponent<TextMeshProUGUI>().text = "Score = " + (maxScore - (Mathf.Round(timer * 100)/100f * scoreMultiplier)).ToString();
             OnFinish.Invoke();
+        }
+
+        //Check collision with checkpoint
+        if (collision.gameObject.name.Contains("Checkpoint"))
+        {
+            collision.gameObject.SetActive(false);
+            checkpoint = collision.gameObject.transform.position;
         }
     }
     void Update()
     {
         //Check if player fell off the screen
-        if (circleCollider.bounds.center.y < deathBoundary)
+        if (circleCollider.bounds.center.y < deathBoundary && !isDead)
         {
             isDead = true;
-            OnDeath.Invoke();
+            //OnDeath.Invoke();
+            Respawn();
         }
 
-        if (!isDead && !isFinished)
+        if (!isDead)
         {
-            horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-
-            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-            //Jump
-            if (Input.GetButtonDown("Jump"))
+            if (!isFinished)
             {
-                jump = true;
-                animator.SetBool("isJumping", true);
+                horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+                animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+                //Jump
+                if (Input.GetButtonDown("Jump"))
+                {
+                    jump = true;
+                    animator.SetBool("isJumping", true);
+                }
+
+                timer += Time.deltaTime;
+                timerObj.GetComponent<TextMeshProUGUI>().text = ((Mathf.RoundToInt(timer * 100))/100.0f).ToString();
+            }
+
+            //Check if player is grounded to reset animation
+            if (IsGrounded())
+            {
+                animator.SetBool("isJumping", false);
             }
         }
 
-        //Check if player is grounded to reset animation
-        if (IsGrounded())
-        {
-            animator.SetBool("isJumping", false);
-        }
+        
     }
     private bool IsGrounded()
     {
@@ -100,7 +135,15 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         //Move player
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        if (!isDead)
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        }
+    }
+    void Respawn()
+    {
+        playerTransform.position = checkpoint;
+        isDead = false;
     }
 }
